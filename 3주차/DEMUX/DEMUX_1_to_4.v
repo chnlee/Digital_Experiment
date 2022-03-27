1c8dfef050e3129046f5516de4091e61ca40cb5c
chnlee <84317436+chnlee@users.noreply.github.com>
2022-03-27 23:35:40 +0900

    Add files via upload

--- 3주차/DEMUX/DEMUX_1_to_4.v
+++ 3주차/DEMUX/DEMUX_1_to_4.v
@@ -19,23 +19,23 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module DEMUX_1_to_4(
+module DEMUX_1_to_4( // Demux에 대한 모듈 생성
     in, control, out
     ); 
-    input in;
-    input [2:0] control;
-    output reg [3:0] out;
+    input in; // 1개의 input값을 받는다.
+    input [2:0] control; // 2비트의 control값을 가지며, input이 출력될 output의 위치를 결정한다.
+    output reg [3:0] out; // 4비트의 output값을 가지며, 한개의 element만 1을 가지고 나머지 element는 0을 가진다.
 
-    always@(*)
-    begin 
-        if(control == 2'b00) begin
-           out[0] = in; out[1] = 0; out[2] = 0; out[3] = 0; end
-        else if(control == 2'b01) begin
-           out[0] = 0; out[1] = in; out[2] = 0; out[3] = 0; end
-        else if(control == 2'b10) begin
-           out[0] = 0; out[1] = 0; out[2] = in; out[3] = 0; end
-        else begin
-           out[0] = 0; out[1] = 0; out[2] = 0; out[3] = in; end
-    end
+    always@(*) // always문은 bracket 안에 있는 코드를 실행하는 블록을 의미한다.
+    begin // always 문의 시작
+        if(control == 2'b00) begin  // 입력값인 control이 00일 때
+           out[0] = in; out[1] = 0; out[2] = 0; out[3] = 0; end // out[0]이 입력값인 in을 가지고 나머지는 0을 가진다.
+        else if(control == 2'b01) begin // 입력값인 control이 01일 때
+           out[0] = 0; out[1] = in; out[2] = 0; out[3] = 0; end // out[1]이 입력값인 in을 가지고 나머지는 0을 가진다.
+        else if(control == 2'b10) begin // 입력값인 control이 10일 때
+           out[0] = 0; out[1] = 0; out[2] = in; out[3] = 0; end // out[2]이 입력값인 in을 가지고 나머지는 0을 가진다.
+        else begin // 입력값인 control이 11일 때
+           out[0] = 0; out[1] = 0; out[2] = 0; out[3] = in; end // out[3]이 입력값인 in을 가지고 나머지는 0을 가진다.
+    end // always 문의 끝
     
 endmodule
--- 3주차/DEMUX/topModule.v
+++ 3주차/DEMUX/topModule.v
@@ -20,13 +20,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module LED_DEMUX(sw, led);
-    input [1:0] sw;
-    output [3:0] led;
-    wire [2:0] cont;
-    parameter DEMUX_input = 1'b1;
-    wire [3:0] DEMUX_output;
+module LED_DEMUX(sw, led); // 보드에 구현할 모듈을 생성한다.
+    input [1:0] sw; // 보드의 switch이며 2개이므로, constraints에 따라 2비트를 할당한다.
+    output [3:0] led; // 보드의 led이며 4개이므로, constraints에 따라 4비트를 할당한다.
+    wire [2:0] cont; // wire를 통해 보드와 연결할 cont값을 지정하고, 이 값에 따라 다른 led에 불이 켜진다. 
+    parameter DEMUX_input = 1'b1; // 기본 DEMUX의 입력값이며, 이 값이 출력값에 할당된다.
+    wire [3:0] DEMUX_output; // wire를 통해 보드와 연결될 출력값이며 4개의 led로 4비트로 구성되어 있다.
     
     // Module Body
+    // 모듈 인스턴스를 통해 DEMUX_1_to_4의 입력값과 출력값을 LED_DEMUX의 input과 ouput에 인스턴스화 한다.
     DEMUX_1_to_4 DEMUX_0(.in(DEMUX_input), .control(sw), .out(led));
 endmodule
