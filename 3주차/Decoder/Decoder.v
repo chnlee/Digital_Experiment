23a364a1c5ae036b7647675b1cd964a7ea4d0b36
chnlee <84317436+chnlee@users.noreply.github.com>
2022-03-27 23:34:20 +0900

    Add files via upload

--- 3주차/Decoder/Decoder.v
+++ 3주차/Decoder/Decoder.v
@@ -21,20 +21,20 @@
 
 
 module Decoder(in, out);
-    input [2:0] in;
-    output reg [7:0] out;
-    always@(*)
-    begin
-        case(in)
-        3'b000 : out <= 8'b00000001;
-        3'b001 : out <= 8'b00000010;
-        3'b010 : out <= 8'b00000100;
-        3'b011 : out <= 8'b00001000;
-        3'b100 : out <= 8'b00010000;
-        3'b101 : out <= 8'b00100000;
-        3'b110 : out <= 8'b01000000;
-        3'b111 : out <= 8'b10000000;
-        default : out <= 8'b00000000; 
-        endcase
+    input [2:0] in; //3비트의 input값을 지정한다.
+    output reg [7:0] out; // 3비트이므로 총 8가지의 경우의 수가 나온다. 따라서 8비트 output을 지정한다.
+    always@(*) // always문을 통해 코드를 실행한다.
+    begin 
+        case(in) // 총 9가지 경우가 나오기 때문에 if-else문보다 case문을 이용하여 코드를 작성한다.
+        3'b000 : out <= 8'b00000001; // input값이 000일 때 output은 8비트 00000001이다.
+        3'b001 : out <= 8'b00000010; // input값이 001일 때 output은 8비트 00000010이다.
+        3'b010 : out <= 8'b00000100; // input값이 010일 때 output은 8비트 00000100이다.
+        3'b011 : out <= 8'b00001000; // input값이 011일 때 output은 8비트 00001000이다.
+        3'b100 : out <= 8'b00010000; // input값이 100일 때 output은 8비트 00010000이다.
+        3'b101 : out <= 8'b00100000; // input값이 101일 때 output은 8비트 00100000이다.
+        3'b110 : out <= 8'b01000000; // input값이 110일 때 output은 8비트 01000000이다.
+        3'b111 : out <= 8'b10000000; // input값이 111일 때 output은 8비트 10000000이다.
+        default : out <= 8'b00000000; // input값이 위 case들과 다 적합하지 않을 때 enable이 0으로 설정되어 기본값인 8비트 00000000을 가진다.
+        endcase 
     end
 endmodule 
--- 3주차/Decoder/Decoder_RGB.v
+++ 3주차/Decoder/Decoder_RGB.v
@@ -20,26 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module Decoder_RGB(
-        sw, btn, led4_r, led4_g, led4_b, led5_r, led5_g, led5_b
+        sw, btn, led4_r, led4_g, led4_b, led5_r, led5_g, led5_b // 보드에서 필요한 element들을 모두 선언한다.
     );
-    input [1:0] sw;
-    input [3:0] btn;
-    output led4_r, led4_g, led4_b, led5_r, led5_g, led5_b;
+    input [1:0] sw; // switch는 2개로 구성되어 있으므로 2비트로 선언한다.
+    input [3:0] btn; // 버튼은 4개로 구성되어 있으므로 4비트로 선언하지만 실제 사용할 버튼은 1개이다.
+    output led4_r, led4_g, led4_b, led5_r, led5_g, led5_b; // Decoder를 나타내기 위해 LED 6개를 출력값으로 선언한다.
     
     // Module Body
-    wire [2:0] decoder_in;
-    wire [5:0] decoder_out;
+    wire [2:0] decoder_in; // wire를 통해서 switch, 버튼과 연결하기 위해 3비트 값인 decoder_in을 입력값으로 선언한다.
+    wire [5:0] decoder_out; // wire를 통해서 led와 연결하기 위해 6비트 값인 decoder_out을 출력값으로 선언한다.
     
-    assign decoder_in[2] = sw[1];
-    assign decoder_in[1] = sw[0];
-    assign decoder_in[0] = btn[0];
+    assign decoder_in[2] = sw[1]; // 입력값에 스위치 값을 할당한다.
+    assign decoder_in[1] = sw[0]; // 입력값에 스위치 값을 할당한다.
+    assign decoder_in[0] = btn[0]; // 입력값에 버튼 값을 할당한다.
     
-    assign led4_r = decoder_out[5];
-    assign led4_g = decoder_out[4];
-    assign led4_b = decoder_out[3];
-    assign led5_r = decoder_out[2];
-    assign led5_g = decoder_out[1];
-    assign led5_b = decoder_out[0];
+    // 강의안의 표에 따르면 led4_r가 가장 높은 비트, led5_b가 가장 낮은 비트로 색이 구현되어 있는 것을 확인할 수 있다.
+    assign led4_r = decoder_out[5]; // 가장 높은 index로 출력값을 led4_r에 할당한다.
+    assign led4_g = decoder_out[4]; // 그 다음 index로 출력값을 led4_g에 할당한다.
+    assign led4_b = decoder_out[3]; // 그 다음 index로 출력값을 led4_b에 할당한다.
+    assign led5_r = decoder_out[2]; // 그 다음 index로 출력값을 led5_r에 할당한다.
+    assign led5_g = decoder_out[1]; // 그 다음 index로 출력값을 led5_g에 할당한다.
+    assign led5_b = decoder_out[0]; // 마지막 index로 출력값을 led5_b에 할당한다.
     
+    // module instantiation을 통해 RGB_Decoder 모듈에서 선언한 3비트의 입력값을 스위치(2개)와 1번째 버튼에 할당하고 6비트의 출력값과 out을 인스턴스화하여 객체를 생성한다.
+    // RGB_Decoder의 각 출력값이 led에 할당되어 서로 다른 8가지의 색을 구현할 수 있다.
     RGB_Decoder decoder_0(.in({sw,btn[0]}), .out(decoder_out));
 endmodule
